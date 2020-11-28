334c5396b50b35c354062cedb67d5509910e00fe
Saurav-Rawat <rawatsaurav889@gmail.com>
2020-11-29 02:52:55 +0530

    env variable fix

--- confi/lib/sms.js
+++ confi/lib/sms.js
@@ -1,8 +1,8 @@
 const twilio = require("twilio");
 export const sendSms = (data, callback) => {
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  const appHash = process.env.OTP_APP_HASH;
+  const accountSid = "ACdc570419de374455d6bfd24b9bd564ec";
+  const authToken = "61e5f1610a677e5eb6f5aef51af34751";
+  //   const appHash = process.env.OTP_APP_HASH;
   const client = twilio(accountSid, authToken);
   const { message, number } = data;
   client.messages
